dd5895bc73cabde3ec82f74b9ce4e4aa6ee67f1a
Prabhjot Singh <97115364+PrabhjotxSingh@users.noreply.github.com>
2024-11-15 15:08:57 -0500

    Updates to the API

--- better-yahoo/src/lib/api.js
+++ better-yahoo/src/lib/api.js
@@ -1,6 +1,7 @@
-export async function fetchTrendingNews() {
+// Reese will be using this one
+export async function fetchPopular() {
   const apiKey = "fkzmGJWZU84L8CddLeZXDTKgSgByox3R";
-  const API_URL = `https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=${apiKey}`;
+  const API_URL = `https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${apiKey}`;
 
   try {
     const response = await fetch(API_URL);
@@ -18,6 +19,31 @@ export async function fetchTrendingNews() {
     }));
   } catch (error) {
     console.error("Error fetching trending news:", error);
+    return [];
+  }
+}
+
+// Prabh will be using this one
+export async function fetchTopStories() {
+  const apiKey = "fkzmGJWZU84L8CddLeZXDTKgSgByox3R";
+  const API_URL = `https://api.nytimes.com/svc/topstories/v2/world.json?api-key=${apiKey}`;
+
+  try {
+    const response = await fetch(API_URL);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+    return data.results.map((item) => ({
+      title: item.title,
+      image: item.multimedia?.[0]?.url || "",
+      subheading: item.abstract,
+      link: item.url,
+    }));
+  } catch (error) {
+    console.error("Error fetching top stories:", error);
     return [];
   }
 }
