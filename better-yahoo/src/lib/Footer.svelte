3f8496db67a003e4f9a094037660d2d3d7e78ce9
24varshag <24varshag@gmail.com>
2024-11-18 17:47:15 -0500

    Updated icons for footer

--- better-yahoo/src/lib/Footer.svelte
+++ better-yahoo/src/lib/Footer.svelte
@@ -1,5 +1,17 @@
 <div class="footer-container">
-  <p>Yahoo</p>
+  <!-- Social Media Icons -->
+  <div class="social-icons">
+    <a href="https://twitter.com" target="_blank" aria-label="Twitter">
+      <img src="https://cdn-icons-png.flaticon.com/512/733/733579.png" alt="Twitter" class="icon" />
+    </a>
+    <a href="https://facebook.com" target="_blank" aria-label="Facebook">
+      <img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" alt="Facebook" class="icon" />
+    </a>
+    <a href="https://instagram.com" target="_blank" aria-label="Instagram">
+      <img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" alt="Instagram" class="icon" />
+    </a>
+  </div>
+
   <div class="footer-links">
     <a href="#example">Terms and Privacy Policy</a>
     <a href="#example">Privacy Dashboard</a>
@@ -11,6 +23,7 @@
     <br />
     <a href="#example">Feedback</a>
   </div>
+
   <div class="footer-note">
     <p>© 2024 Yahoo. All rights reserved.</p>
   </div>
@@ -20,12 +33,35 @@
   .footer-container {
     margin-top: 50px;
     margin-bottom: 20px;
-    width: 100%;
     text-align: center;
-    padding: 20x;
+    padding: 20px;
+  }
+
+  .social-icons {
+    margin-bottom: 10px;
+  }
+
+  .social-icons a {
+    margin: 0 10px;
+  }
+
+  .icon {
+    width: 24px;
+    height: 24px;
+    vertical-align: middle;
   }
 
   .footer-links a {
     color: black;
+    text-decoration: none;
+    margin: 0 10px;
+  }
+
+  .footer-links a:hover {
+    text-decoration: underline;
+  }
+
+  .footer-note {
+    margin-top: 15px;
   }
 </style>
